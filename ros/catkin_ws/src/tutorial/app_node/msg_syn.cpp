466069cfa09009c688f80305625c5c96e5d2a830
hanshuo <hanshuo@seedland.com>
2021-08-05 16:28:21 +0800

    add update

--- ros/catkin_ws/src/tutorial/app_node/msg_syn.cpp
+++ ros/catkin_ws/src/tutorial/app_node/msg_syn.cpp
@@ -2,6 +2,12 @@
 #include <boost/thread/thread.hpp>
 #include <iostream>
 #include <ros/duration.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_ros/transforms.h>
 
 namespace climbing_robot
 {
@@ -25,11 +31,19 @@ namespace climbing_robot
     {
         ROS_INFO("pointcloud stamp value is: %f", ori_pointcloud->header.stamp.toSec());
         ROS_INFO("pose stamp value is: %f", ori_image->header.stamp.toSec());
-        // ROS_INFO("joint stamp value is: %f" , ori_joint->header.stamp.toSec());
 
         msg_syn_bag.write("/pointcloud", ori_pointcloud->header.stamp, *ori_pointcloud);
         msg_syn_bag.write("/image", ori_pointcloud->header.stamp, *ori_image);
         // msg_syn_bag.write("/joint" , ori_joint->header.stamp , *ori_joint);
+
+        pcl::PCLPointCloud2 pcl_pc2;
+        pcl_conversions::toPCL(*ori_pointcloud, pcl_pc2);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::fromPCLPointCloud2(pcl_pc2, *temp_cloud);
+
+        std::cout << "temp_cloud size: " << temp_cloud->points.size() << std::endl;
+        pcl::PCDWriter writer;
+        writer.write("/home/han/project/learn_slam/ros/catkin_ws/src/tutorial/build/test.pcd", *temp_cloud);
     }
 
 } //namespace climbing_robot
