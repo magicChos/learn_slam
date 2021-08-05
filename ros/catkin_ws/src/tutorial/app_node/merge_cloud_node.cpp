f664d265433e00ec9f7887ab7a3613018a15d0e7
hanshuo <hanshuo@seedland.com>
2021-08-05 21:50:54 +0800

    merge cloud

---
+++ ros/catkin_ws/src/tutorial/app_node/merge_cloud_node.cpp
@@ -0,0 +1,75 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <rosbag/bag.h>
+#include <rosbag/bag_player.h>
+#include <vector>
+
+using namespace std;
+typedef pcl::PointXYZRGB PointType;
+vector<sensor_msgs::PointCloud2> lidar_datas;
+
+void loadPointcloudFromROSBag(const string &bag_path)
+{
+    ROS_INFO("Start to load the rosbag %s", bag_path.c_str());
+    rosbag::Bag bag;
+    try
+    {
+        bag.open(bag_path, rosbag::bagmode::Read);
+    }
+    catch (rosbag::BagException e)
+    {
+        ROS_ERROR_STREAM("LOADING BAG FAILED: " << e.what());
+        return;
+    }
+
+    vector<string> types;
+    types.push_back(string("sensor_msgs/PointCloud2")); // message title
+    rosbag::View view(bag, rosbag::TypeQuery(types));
+
+    for (const rosbag::MessageInstance &m : view)
+    {
+        sensor_msgs::PointCloud2 livoxCloud = *(m.instantiate<sensor_msgs::PointCloud2>()); // message type
+        lidar_datas.push_back(livoxCloud);
+    }
+}
+
+int main(int argc, char **argv)
+{
+
+    ros::init(argc, argv, "merge_cloud");
+    ros::NodeHandle n;
+
+    ROS_INFO("\033[1;32m---->\033[0m merge_cloud node Started. ");
+
+    std::string input_bag_path = "/home/han/2021-08-05-17-12-12.bag";
+    loadPointcloudFromROSBag(input_bag_path);
+    uint64_t num = 0;
+    pcl::PointCloud<PointType>::Ptr result_cloud(new pcl::PointCloud<PointType>);
+    while (n.ok())
+    {
+        ros::spinOnce();
+        if (num < lidar_datas.size())
+        {
+            std::cout << "num: " << num << std::endl;
+            pcl::PointCloud<PointType>::Ptr cloud(new pcl::PointCloud<PointType>);
+            pcl::fromROSMsg(lidar_datas[num], *cloud);
+            std::cout << "cloud size: " << cloud->points.size() << std::endl;
+
+            *result_cloud += *cloud;
+            std::cout << "result cloud size: " << result_cloud->points.size() << std::endl;
+            ++num;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    pcl::PCDWriter writer;
+    writer.write("/home/han/project/learn_slam/ros/catkin_ws/src/tutorial/build/merge_cloud.pcd", *result_cloud);
+
+    return 0;
+}
